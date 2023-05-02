4e28bd1f248c50a88bcb99a181bd918cdee46678
Gareth Miller <6932794+gar-mil@users.noreply.github.com>
2023-05-02 13:57:49 -0500

    Bootstrap initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
-import './globals.css'
+import Head from "next/head";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect } from "react";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
