dad97f4193213b190cf3b31ec95e50a631e4ed84
Gareth Miller <6932794+gar-mil@users.noreply.github.com>
2023-05-04 01:54:00 -0500

    Intro text

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,9 @@ export default function Index()
       <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']} minBreakpoint="xxs">
         <Container className="innerContainer innerContainer-bg no-padding vh100">
           <Row>
-            <Col id="errorDisplay"></Col>
+            <Col className="introCol">
+              <p><strong>Cinématographe</strong> is a React/Nextjs/GraphQL application that consumes the <a href="https://www.omdbapi.com/" target='_blank'>OMDb API</a> and displays information to the user about movies that they have searched for.</p>
+            </Col>
           </Row>
           <Row>
             <Col>
